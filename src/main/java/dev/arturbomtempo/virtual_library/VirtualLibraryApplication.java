d2349045f72ab525099828a4468662aac0b4921c
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-05-01 18:13:57 -0300

    Initial commit

---
+++ src/main/java/dev/arturbomtempo/virtual_library/VirtualLibraryApplication.java
@@ -0,0 +1,13 @@
+package dev.arturbomtempo.virtual_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/arturbomtempo/virtual_library/VirtualLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arturbomtempo.virtual_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
