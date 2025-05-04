a21fc405ae95d6dd4db553fcebf3dd76297d8205
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-05-04 16:57:44 -0300

    feat: add enum for category types

---
+++ src/main/java/dev/arturbomtempo/virtual_library/book/model/enumeration/Category.java
@@ -0,0 +1,16 @@
+package dev.arturbomtempo.virtual_library.book.model.enumeration;
+
+public enum Category {
+    ROMANCE,
+    SCIENCE,
+    HISTORY,
+    TECHNOLOGY,
+    BIOGRAPHY,
+    SELF_HELP,
+    PHILOSOPHY,
+    RELIGION,
+    CHILDREN,
+    COMICS,
+    MAGAZINE,
+    INTERVIEW
+}
